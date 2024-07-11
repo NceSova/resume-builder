914556b9d294b0239335eb97d2fb10fb5f665412
Sova <108209320+NceSova@users.noreply.github.com>
2024-07-11 20:07:05 +0300

    feat: starting page for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, world!</h1>;
+  return (
+    <div className="bg flex h-screen w-screen flex-col items-center justify-center bg-slate-800">
+      <h1 className="text-5xl text-blue-100">Привет, Мир!</h1>
+    </div>
+  );
 }
 
 export default App;
